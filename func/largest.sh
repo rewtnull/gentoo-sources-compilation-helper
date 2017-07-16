8414975d0768c7a582fe3421f66324a5bb3e1534
Marcus Hoffren <marcus@harikazen.com>
2017-07-16 03:28:22 +0200

    Add files via upload

---
+++ func/largest.sh
@@ -0,0 +1,12 @@
+# largest(${array[@]} => $largest)
+largest() {
+    local array=("${@}")
+    if (( ${#array[@]} > "1" )); then # If array consists of one single element it's already the largest one
+	for (( i=0; i < ${#array[@]}; i++ )); do
+	    gtoe "${array[${i}]}" "${largest_element}" && largest_element="${array[${i}]}"
+	done
+    else
+	largest_element="${array[0]}"
+    fi
+    echo "${largest_element}"
+}
