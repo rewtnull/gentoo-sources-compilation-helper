c2e907cfa141424381cb5ac3e4996fd75fa31b1a
Marcus Hoffren <marcus@harikazen.com>
2017-07-25 04:55:36 +0200

    See Changelog

--- func/usage.sh
+++ func/usage.sh
@@ -1,9 +1,11 @@
 usage() {
-    echo "Usage: ${0##*/} [--help|-h] [--version|-v] [--kernel|-k <version>] [--yestoall|-y]"
+    echo "Usage: ${0##*/} [--help|-h] [--version|-v] [--kernel|-k <version>]"
+    echo "              [--initramfs|-i] [--][--yestoall|-y]"
     echo ""
     echo "-h, --help        Display this help"
     echo "-v, --version     Display version and exit"
     echo "-k, --kernel      kernel version in format linux-<version>-gentoo[<-r<1-9>>]"
+    echo "-i, --initramfs   Generate initramfs"
     echo "-y, --yestoall    Automatically answer yes to all questions"
     echo ""
     echo ""
