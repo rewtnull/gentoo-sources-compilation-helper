f618f4d8c60b22ea176266e79063db4217a4351a
Marcus Hoffren <marcus@harikazen.com>
2017-07-28 03:02:21 +0200

    See Changelog

--- func/largest.sh
+++ func/largest.sh
@@ -1,8 +1,8 @@
-# largest(${array[@]} => $largest)
+# largest( ${array[@]} ) => $largest
 largest() {
     local array=("${@}")
     if (( ${#array[@]} > "1" )); then # If array consists of one single element it's already the largest one
-	for (( i=0; i < ${#array[@]}; i++ )); do
+	for (( i = 0; i < ${#array[@]}; i++ )); do
 	    gtoe "${array[${i}]}" "${largest_element}" && largest_element="${array[${i}]}"
 	done
     else
