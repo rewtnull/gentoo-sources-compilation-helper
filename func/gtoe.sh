f6b5bb6bc6337311ddfb207b726cd54a09700501
Marcus Hoffren <marcus@harikazen.com>
2017-09-17 22:13:31 +0200

    See Changelog

--- func/gtoe.sh
+++ func/gtoe.sh
@@ -5,8 +5,8 @@ gtoe() {
     local second="${2}"
     local re="^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]$"
 
-    [[ ${first} =~ ${re} ]] && first=$(echo ${first} | sed 's/.$/0&/')
-    [[ ${second} =~ ${re} ]] && second=$(echo ${second} | sed 's/.$/0&/')
+    [[ ${first} =~ ${re} ]] && first=$(echo ${first} | perl -pe 's/(.$)/0\1/')
+    [[ ${second} =~ ${re} ]] && second=$(echo ${second} | perl -pe 's/(.$)/0\1/')
 
     [[ "${first}" > "${second}" || "${first}" == "${second}" ]]
 }
