17c647091484b3d0a643cbe17696f3d41153c845
Marcus Hoffren <marcus@harikazen.com>
2017-11-07 00:42:18 +0100

    See Changelog

--- func/version.sh
+++ func/version.sh
@@ -1,6 +1,6 @@
 version() {
     local scrname="Gentoo genkernel Compilation Helper"
-    local scrver="0.16"
+    local scrver="0.17"
     local scrauth="Marcus Hoffren"
     local authnick="dMG/Up Rough^Divine Stylers^TrueSchool"
     local scrcontact="marcus@harikazen.com"
