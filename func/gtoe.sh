bfb72a73a920a3fc25b12fad0f2bb436348cc905
Marcus Hoffren <marcus@harikazen.com>
2017-11-07 00:40:39 +0100

    See Changelog

--- func/gtoe.sh
+++ func/gtoe.sh
@@ -1,12 +1,9 @@
-# Greater than or equal for string comparisons
-
 gtoe() {
     local first="${1}"
     local second="${2}"
-    local re="^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]$"
 
-    [[ ${first} =~ ${re} ]] && first=$(echo ${first} | perl -pe 's/(.$)/0\1/')
-    [[ ${second} =~ ${re} ]] && second=$(echo ${second} | perl -pe 's/(.$)/0\1/')
+    first="$(addzero "${first}")" # if there's only one digit at the end of the version string, add a 0 before it
+    second="$(addzero "${second}")"
 
-    [[ "${first}" > "${second}" || "${first}" == "${second}" ]]
+    [[ "$first}" > "${second}" || "${first}" == "${second}" ]]
 }
--- func/largest.sh
+++ func/largest.sh
@@ -1,7 +1,9 @@
 # largest( ${array[@]} ) => $largest
 
 largest() {
+
     local array=("${@}")
+
     if (( ${#array[@]} > "1" )); then # If array consists of one single element it's already the largest one
 	for (( i = 0; i < ${#array[@]}; i++ )); do
 	    gtoe "${array[${i}]}" "${largest_element}" && largest_element="${array[${i}]}"
@@ -10,4 +12,5 @@ largest() {
 	largest_element="${array[0]}"
     fi
     echo "${largest_element}"
+
 }; unset largest_element i
