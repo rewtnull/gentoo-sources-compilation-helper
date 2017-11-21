d1c6a1c86d2f3370cf9bfa6e04a8a5518a953c4e
Marcus Hoffren <marcus@harikazen.com>
2017-11-22 00:52:03 +0100

    Updated to v0.18. See Changelog

--- func/gtoe.sh
+++ func/gtoe.sh
@@ -5,5 +5,5 @@ gtoe() {
     first="$(addzero "${first}")" # if there's only one digit at the end of the version string, add a 0 before it
     second="$(addzero "${second}")"
 
-    [[ "$first}" > "${second}" || "${first}" == "${second}" ]]
+    [[ ${first} > "${second}" || ${first} == "${second}" ]]
 }
--- func/version.sh
+++ func/version.sh
@@ -1,11 +1,10 @@
 version() {
     local scrname="Gentoo genkernel Compilation Helper"
-    local scrver="0.17"
+    local scrver="0.18"
     local scrauth="Marcus Hoffren"
-    local authnick="dMG/Up Rough^Divine Stylers^TrueSchool"
     local scrcontact="marcus@harikazen.com"
     echo "${scrname} v${scrver}"
-    echo "Copyright (C) 2017 ${scrauth} <${authnick}>."
+    echo "Copyright (C) 2017 ${scrauth}"
     echo "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>."
     echo "This is free software: you are free to change and redistribute it."
     echo "There is NO WARRANTY, to the extent permitted by law."
